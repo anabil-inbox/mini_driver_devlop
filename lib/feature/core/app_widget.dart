4afa74f17f07a35e732bb3c898a7f58d9d09cf27
osama_mohamed98 <om107547@gmail.com>
2022-01-24 15:13:58 +0300

    change on getUserDate ?

--- lib/feature/core/app_widget.dart
+++ lib/feature/core/app_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/route_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:inbox_driver/feature/view/screens/auth/splash/splash.dart';
 import 'package:inbox_driver/feature/view/screens/details/order_details_started_screen.dart';
 import 'package:inbox_driver/util/app_color.dart';
 import 'package:inbox_driver/util/app_dimen.dart';
@@ -75,7 +76,7 @@ class AppWidget extends StatelessWidget {
             TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
           }),
         ),
-       home: const OrderDetailsStarted()
+       home: const SplashScreen()
         // home: const ProfileScreen(),
       ),
     );
