72135c7758cbde7b0ef08f8a780807c7407b3c53
monther <m.abuabedallah@ahdtech.com>
2022-03-17 11:52:38 +0200

    Mearge

--- lib/network/api/feature/home_helper.dart
+++ lib/network/api/feature/home_helper.dart
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> c83ef03f34e65bc1a7239b4d7a219fc6f85eca1f
 import 'package:inbox_driver/feature/model/home/emergencey/emergency_case.dart';
 import 'package:inbox_driver/feature/model/home/sales_data.dart';
 import 'package:inbox_driver/feature/model/home/task_model.dart';
